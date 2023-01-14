0e773d20d465b0be09ee88b7ed5650c6ed961dfd
cspencer <cspencernd@gmail.com>
2023-01-14 03:55:43 -0500

    add CollectionPreview to exports

--- src/components/Products/index.tsx
+++ src/components/Products/index.tsx
@@ -4,4 +4,4 @@ export default function ProductGrid({ children }: Children) {
     return <ul className="layout-grid">{children}</ul>
 }
 
-export { ProductPreview } from "./Product"
+export { CollectionPreview, ProductPreview } from "./Product"
