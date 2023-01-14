7476a78e9926522752bc52a734e94834bc3e79af
cspencer <cspencernd@gmail.com>
2023-01-14 05:08:11 -0500

    add images to dependency array

--- src/utils/useProductImages.tsx
+++ src/utils/useProductImages.tsx
@@ -4,6 +4,7 @@ import { ProductPreview } from "~/components/Products"
 
 export function useProductImages(images: JSX.Element[]) {
     const [productImages, setProductImages] = useState<JSX.Element[]>([])
+
     useEffect(() => {
         const groupedImages = groupBy(
             images,
@@ -11,12 +12,12 @@ export function useProductImages(images: JSX.Element[]) {
         )
         const groupedImagesArray = Object.entries(groupedImages)
 
-        const productPreviews = groupedImagesArray.map((product, i) => {
+        const productPreviews = groupedImagesArray.map((product) => {
             return <ProductPreview product={product} key={product[1][0].key} />
         })
 
         setProductImages(productPreviews)
-    }, []) // eslint-disable-line
+    }, [images])
 
     return productImages
 }
