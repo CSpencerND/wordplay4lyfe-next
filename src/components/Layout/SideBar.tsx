7068de01dbfaa853ed20cb70babfd2b6a8438b77
cspencer <cspencernd@gmail.com>
2023-01-17 17:44:12 -0500

    style active link

--- src/components/Layout/SideBar.tsx
+++ src/components/Layout/SideBar.tsx
@@ -1,31 +1,66 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export const SideBar = ({ handleClick }: { handleClick: () => void }) => {
+    const pathname = useRouter().pathname
+
     return (
         <div className="drawer-side mt-[5rem]" onClick={handleClick}>
             <label className="drawer-overlay !cursor-default"></label>
-            <ul className="menu p-4 w-max text-base whitespace-nowrap">
+            <ul className="menu w-max whitespace-nowrap p-4 text-base">
                 <li>
-                    <Link href="/">Home</Link>
+                    <Link className={pathname === "/" ? "active" : ""} href="/">
+                        Home
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/collections">Collections Directory</Link>
+                    <Link
+                        className={pathname === "/collections" ? "active" : ""}
+                        href="/collections"
+                    >
+                        Collections Directory
+                    </Link>
                 </li>
                 {/* <li className="menu-title"><span>Collections</span></li> */}
                 <li>
-                    <Link href="/collections/summer-22">Summer 2022</Link>
+                    <Link
+                        className={pathname === "/collections/summer-22" ? "active" : ""}
+                        href="/collections/summer-22"
+                    >
+                        Summer 2022
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/collections/staff-picks">Staff Picks</Link>
+                    <Link
+                        className={pathname === "/collections/staff-picks" ? "active" : ""}
+                        href="/collections/staff-picks"
+                    >
+                        Staff Picks
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/collections/mindset">Mindset Tees</Link>
+                    <Link
+                        className={pathname === "/collections/mindset" ? "active" : ""}
+                        href="/collections/mindset"
+                    >
+                        Mindset Tees
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/collections/creative-minds">Creative Minds</Link>
+                    <Link
+                        className={pathname === "/collections/creative-minds" ? "active" : ""}
+                        href="/collections/creative-minds"
+                    >
+                        Creative Minds
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/collections/lifestyle">Lifestyle Tess</Link>
+                    <Link
+                        className={pathname === "/collections/lifestyle" ? "active" : ""}
+                        href="/collections/lifestyle"
+                    >
+                        Lifestyle Tess
+                    </Link>
                 </li>
             </ul>
         </div>
